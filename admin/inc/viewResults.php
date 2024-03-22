52674c74f38b471ee50227c97074a64a81651aad
jeelanibaba <139853132+jeelanibaba@users.noreply.github.com>
2024-03-23 02:24:19 +0530

    Update viewResults.php

--- admin/inc/viewResults.php
+++ admin/inc/viewResults.php
@@ -1,26 +1,18 @@
 <?php 
     $election_id = $_GET['viewResult'];
-
-
 ?>
 
 <div class="row my-3">
     <div class="col-12">
-    <!-- <h3 style="text-align: center; font-weight: bold;">Election Results</h3> -->
-    <div style="text-align: center;">
-    <div style="background-color: #f0f0f0; border: 1px solid #000; padding: 10px; width: 20%; margin: 0 auto;">
-        <h3 style="font-weight: bold; margin: 0;">Election Results</h3>
-    </div>
-</div>
-
-
-
+        <div style="text-align: center;">
+            <div style="background-color: #f0f0f0; border: 1px solid #000; padding: 10px; width: auto; margin: 0 auto;">
+                <h3 style="font-weight: bold; margin: 0;">Election Results</h3>
+            </div>
+        </div>
 
         <?php 
         $fetchingActiveElections = mysqli_query($db, "SELECT * FROM elections WHERE id = '". $election_id ."'") or die(mysqli_error($db));
-        $totalActiveElections = mysqli_num_rows($fetchingActiveElections);
-
-        if ($totalActiveElections > 0) {
+        if ($totalActiveElections = mysqli_num_rows($fetchingActiveElections) > 0) {
             while ($data = mysqli_fetch_assoc($fetchingActiveElections)) {
                 $election_id = $data['id'];
                 $election_topic = $data['election_topic'];
@@ -34,57 +26,51 @@
                     $fetchWinnerDetailsQuery = mysqli_query($db, "SELECT * FROM candidate_details WHERE id = $winner_candidate_id") or die(mysqli_error($db));
                     $winnerDetails = mysqli_fetch_assoc($fetchWinnerDetailsQuery);
                     $winnerName = $winnerDetails['candidate_name'];                
-                    echo "<h4>$winnerName has been elected as $election_topic </h4>";
-                    echo "<h4>Congratulations $winnerName 🎉👏🥳</h4>";
-
+                    echo "<h4 style='text-align: center;'>$winnerName has been elected as $election_topic </h4>";
+                    echo "<h4 style='text-align: center;'>Congratulations $winnerName 🎉👏🥳</h4>";
                 }
-                
-                
-                
         ?>
-                <table class="table">
-                    <thead>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th colspan="3" class="bg-green text-white"><h5 style="text-align: center;">ELECTION TOPIC: <?php echo strtoupper($election_topic); ?></h5></th>
+                    </tr>
+                    <tr>
+                        <th>Photo</th>
+                        <th>Candidate Details</th>
+                        <th># of Votes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php 
+                    $fetchingCandidates = mysqli_query($db, "SELECT * FROM candidate_details WHERE election_id = '". $election_id ."'") or die(mysqli_error($db));
+                    while ($candidateData = mysqli_fetch_assoc($fetchingCandidates)) {
+                        $candidate_photo = $candidateData['candidate_photo'];
+                        $fetchingVotes = mysqli_query($db, "SELECT * FROM votings WHERE candidate_id = '". $candidateData['id'] . "'") or die(mysqli_error($db));
+                        $totalVotes = mysqli_num_rows($fetchingVotes);
+                ?>
                         <tr>
-                            <th colspan="4" class="bg-green text-white"><h5> ELECTION TOPIC: <?php echo strtoupper($election_topic); ?></h5></th>
+                            <td><img src="<?php echo $candidate_photo; ?>" class="img-fluid" style="max-width: 100px; height: auto;"></td>
+                            <td><?php echo "<b>" . $candidateData['candidate_name'] . "</b><br />" . $candidateData['candidate_details']; ?></td>
+                            <td><?php echo $totalVotes; ?></td>
                         </tr>
-                        <tr>
-                            <th> Photo </th>
-                            <th> Candidate Details </th>
-                            <th> # of Votes </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php 
-                        $fetchingCandidates = mysqli_query($db, "SELECT * FROM candidate_details WHERE election_id = '". $election_id ."'") or die(mysqli_error($db));
-
-                        while ($candidateData = mysqli_fetch_assoc($fetchingCandidates)) {
-                            $candidate_id = $candidateData['id'];
-                            $candidate_photo = $candidateData['candidate_photo'];
-
-                            $fetchingVotes = mysqli_query($db, "SELECT * FROM votings WHERE candidate_id = '". $candidate_id . "'") or die(mysqli_error($db));
-                            $totalVotes = mysqli_num_rows($fetchingVotes);
-                    ?>
-                            <tr>
-                                <td> <img src="<?php echo $candidate_photo; ?>" class="candidate_photo"> </td>
-                                <td><?php echo "<b>" . $candidateData['candidate_name'] . "</b><br />" . $candidateData['candidate_details']; ?></td>
-                                <td><?php echo $totalVotes; ?></td>
-                            </tr>
-                    <?php
-                        }
-                    ?>
-                    </tbody>
-                </table>
+                <?php
+                    }
+                ?>
+                </tbody>
+            </table>
+        </div>
         <?php
             }
         } else {
-            echo "No such active election.";
+            echo "<p style='text-align: center;'>No such active election.</p>";
         }
         ?>
-    </div>
-</div>
-            <hr>
-            <h3>Voting Details</h3>
-            <?php 
+
+        <hr>
+        <h3 style="text-align: center;">Voting Details</h3>
+         <?php 
                 $fetchingVoteDetails = mysqli_query($db, "SELECT * FROM votings WHERE election_id = '". $election_id ."'");
                 $number_of_votes = mysqli_num_rows($fetchingVoteDetails);
 
@@ -92,14 +78,18 @@
                 {
                     $sno = 1;
             ?>
-                    <table class="table">
-                        <tr>
-                            <th>S.No</th>
-                            <th>Voter Name</th>
-                            <th>Voted To</th>
-                            <th>Date </th>
-                            <th>Time</th>
-                        </tr>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>S.No</th>
+                        <th>Voter Name</th>
+                        <th>Voted To</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
 
             <?php
                     while($data = mysqli_fetch_assoc($fetchingVoteDetails))
@@ -149,9 +139,11 @@
 
 
                 ?>
+                </tbody>
             </table>
-            
         </div>
     </div>
+</div>
 
-
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
