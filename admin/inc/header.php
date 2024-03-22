5f465628f38ab03c9e3736d7266a23edfdd467f7
jeelanibaba <139853132+jeelanibaba@users.noreply.github.com>
2024-03-23 02:23:08 +0530

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -22,25 +22,15 @@
 <body>
     
 <div class="container-fluid">
-    <div class="row bg-black text-white">
-        <div class="col-1">
+    <div class="row bg-black text-white align-items-center"> <!-- Added align-items-center for vertical alignment -->
+        <div class="col-lg-1 col-md-2 col-sm-3 col-3"> <!-- Adjusted column sizes for different screen sizes -->
             <div style="width: 80px; height: 80px; background-color: #fff; border-radius: 50%; display: flex; justify-content: center; align-items: center; overflow: hidden;">
-                <img src="../assets/images/logo1.gif" style="width: 100%; height: 100%; object-fit: cover;" alt="Logo in Circular Container" />
+                <img src="../assets/images/logo1.gif" style="width: 100%; height: 100%; object-fit: cover;" alt="Logo in Circular Container" class="img-fluid"/>
             </div>
         </div>
-        <div class="col-11 my-auto">
+        <div class="col-lg-11 col-md-10 col-sm-9 col-9"> <!-- Adjusted column sizes for different screen sizes -->
             <h3>ONLINE VOTING SYSTEM</h3>
             <h4><small style="font-weight: bold; font-size: 18px;">Welcome <?php echo $_SESSION['username']; ?></small></h4>
-
         </div>
     </div>
 </div>
-
-
-
- 
-
-
-
-
-
